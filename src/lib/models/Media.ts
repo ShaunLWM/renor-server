c9aca08c6b72ee4a990c3e53b2c280c66b23b77a
ShaunLWM <shaunlimweimin@gmail.com>
2020-06-05 20:44:54 +0800

    Create media schema for storing files

---
+++ src/lib/models/Media.ts
@@ -0,0 +1,24 @@
+import { Document, model, Schema } from "mongoose";
+
+const MediaSchema: Schema = new Schema({
+	gid: {
+		type: Schema.Types.ObjectId,
+		ref: "Gif",
+	},
+	path: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+});
+
+export interface IMedia extends Document {
+	gid: Schema.Types.ObjectId;
+	path: string;
+	type: String;
+}
+
+export default model<IMedia>("Media", MediaSchema);
